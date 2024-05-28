04775d9b16683f71648abe7351672ee0372ab160
Gabriele Barcella <barcigabri@gmail.com>
2024-05-28 15:21:24 +0200

    fix: added the data depth to the response array

--- lib/OAuth2/OAuth2DeviceCode/OAuth2DeviceCodeManager.php
+++ lib/OAuth2/OAuth2DeviceCode/OAuth2DeviceCodeManager.php
@@ -37,7 +37,7 @@ class OAuth2DeviceCodeManager extends OAuth2Manager
             'client_id' => $this->clientId,
             'scope' => $scopeStr,
         ];
-        $res = $this->executePost($tokenUri, $body);
+        $res = $this->executePost($tokenUri, $body)['data'];
         if (!$res instanceOf OAuth2Error) {
             return new OAuth2DeviceCodeResponse($res['device_code'], $res['user_code'], $res['scope'], $res['verification_uri'], $res['interval'], $res['expires_in']);
         } else {
